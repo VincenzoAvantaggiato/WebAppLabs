1c0ded454926c2f5af81c862889336c6e7f513f4
Vincenzo Avantaggiato <avantaggiatovincenzo01@gmail.com>
2024-05-09 11:30:24 +0200

    lab07

---
+++ Lab07/films/src/App.jsx
@@ -0,0 +1,108 @@
+/*
+ * 01UDFOV Applicazioni Web I / 01TXYOV Web Applications I
+ * Lab 6 - 2024
+ */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App.css';
+import {INITIAL_FILMS} from "./films.mjs";
+
+import dayjs from 'dayjs';
+
+import {useState} from 'react';
+import {Button, Collapse, Col, Container, Row} from 'react-bootstrap/';
+import Filters from './components/Filters';
+import Header from "./components/Header.jsx";
+import FilmList from "./components/FilmList.jsx";
+import FilmForm from './components/FilmForm.jsx';
+import { Film } from './films.mjs';
+
+function App() {
+
+    const [films,setFilms] = useState(INITIAL_FILMS);
+    /**
+     * Defining a structure for Filters
+     * Each filter is identified by a unique name and is composed by the following fields:
+     * - A label to be shown in the GUI
+     * - An ID (equal to the unique name), used as key during the table generation
+     * - A filter function applied before passing the films to the FilmTable component
+     */
+    const filters = {
+        'filter-all': {label: 'All', id: 'filter-all', filterFunction: () => true},
+        'filter-favorite': {label: 'Favorites', id: 'filter-favorite', filterFunction: film => film.favorite},
+        'filter-best': {label: 'Best Rated', id: 'filter-best', filterFunction: film => film.rating >= 5},
+        'filter-lastmonth': {
+            label: 'Seen Last Month',
+            id: 'filter-lastmonth',
+            filterFunction: film => {
+                if (!film?.watchDate) return false;
+                const diff = film.watchDate.diff(dayjs(), 'month');
+                return diff <= 0 && diff > -1;
+            }
+        },
+        'filter-unseen': {label: 'Unseen', id: 'filter-unseen', filterFunction: film => !film?.watchDate}
+    };
+
+    // This state contains the active filter
+    const [activeFilter, setActiveFilter] = useState('filter-all');
+
+    // This is not optimal - better ways will be introduced in the upcoming labs
+    const visibleFilms = films.filter(filters[activeFilter].filterFunction);
+
+    // This state controls the expansion of the sidebar (on small breakpoints only)
+    const [isSidebarExpanded, setIsSidebarExpanded] = useState(false);
+
+    // This state controls the visibility of the form
+    const [formVisible,setFormVisible]=useState('button'); // button, add, edit
+
+    const addFilm = (film) => {
+        setFilms((old_films)=>{
+            const id = Math.max(...old_films.map(f=>f.id))+1;
+            const newFilm = new Film(id,film.title,false,film.date,film.rating);
+            return [...old_films, newFilm]
+        });
+    }
+
+    const updateFilm = (film) => {
+        setFilms(old_films=>old_films.map( f =>{
+            if (f.id==film.id) return new Film(f.id,film.title,f.favorite,film.date,film.rating);
+            else return f;
+        }
+        ))
+    }
+    const [editableFilm,setEditableFilm]= useState('');
+
+    return (
+        <div className="min-vh-100 d-flex flex-column">
+            <Header isSidebarExpanded={isSidebarExpanded} setIsSidebarExpanded={setIsSidebarExpanded}/>
+
+            {/* Main */}
+            <Container fluid className="flex-grow-1 d-flex flex-column">
+                <Row className="flex-grow-1">
+                    <Collapse id="films-filters" in={isSidebarExpanded} className="col-md-3 bg-light d-md-block">
+                        <div className="py-4">
+                            <h5 className="mb-3">Filters</h5>
+                            <Filters items={filters} selected={activeFilter} onSelect={setActiveFilter}/>
+                        </div>
+                    </Collapse>
+                    <Col md={9} className="pt-3">
+                        <h1><span id="filter-title">{filters[activeFilter].label}</span> films</h1>
+                        <FilmList films={visibleFilms} setFormVisible={setFormVisible} setEditableFilm={setEditableFilm}/>
+                        {formVisible==='add' && <FilmForm closeForm={()=>setFormVisible('button')} addFilm={addFilm}></FilmForm>}
+                        {formVisible==='edit' && <FilmForm closeForm={()=>setFormVisible('button')} updateFilm={updateFilm} film={editableFilm} key={editableFilm.id}></FilmForm>}
+                    </Col>
+                </Row>
+
+                {formVisible==='button' &&
+                <Button
+                    variant="primary"
+                    className="rounded-circle fixed-right-bottom"
+                    onClick={()=>setFormVisible('add')}
+                >
+                    <i className="bi bi-plus"></i>
+                </Button>}
+            </Container>
+        </div>);
+}
+
+export default App;
